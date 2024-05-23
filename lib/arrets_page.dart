80f790bc41ff5100c23e5f0384f4793a17c73365
lyndaBrye <lyndabrye01@gmail.com>
2024-05-23 14:13:59 +0200

    structuration du code avec ajout d'arrets

--- lib/arrets_page.dart
+++ lib/arrets_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'maps_page.dart';
+import 'views/maps_page.dart';
 
 class ArretsPage extends StatefulWidget {
   @override
